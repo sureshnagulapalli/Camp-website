43cdc7f2aefce95e85c552937e6bada7c92731bd
suresh <sureshnagulapalli@gmail.com>
2019-09-17 05:02:01 +0000

    add url for DATABASEURL

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ var campgroundRoutes 	= require('./routes/campgrounds'),
 	commentRoutes		= require('./routes/comments'),
 	indexRoutes			= require('./routes/index')
 
-mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/YelpCamp"
+
+mongoose.connect(url, {useNewUrlParser: true});
 //mongoose.connect('mongodb://localhost:27017/YelpCamp', {useNewUrlParser: true});
-//mongoose.connect('mongodb+srv://suresh:!WtC-fMHQ$_773f@cluster0-n9a0x.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
